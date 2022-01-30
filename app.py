77d98337b94445ab9d1209c59f921233fb1db25e
Timmy Bergkvist <timmy.bergqvist.89@gmail.com>
2022-01-30 09:29:30 +0100

    adding database models

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
-from models import db, FileUploadModel
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, send_from_directory, current_app
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+# UPLOAD_FOLDER = '/path/to/the/uploads'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fileArchive.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Initialize database
+db=SQLAlchemy(app)
+
+# Database files model
+class FileUploadModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
+    name=db.Column(db.String(200), nullable=False) # Uploaded buy
+    description = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self) :
+        return '<Name %r>' % self.id
 
 
 # Add files
@@ -22,7 +42,7 @@ def delete(id):
 
 # Update files
 @app.route("/update/<int:id>",methods=['POST', 'GET'])
-def update_todo(id):
+def update(id):
     
     return render_template('update.html')
     
